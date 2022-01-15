75957098bc61ebea90e9ca13d73bd60e3cc3fac2
asifs-cse <asifs.cse@gmail.com>
2022-01-15 16:46:54 +0530

    store value and print using vector

---
+++ vector1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<vector>
+
+using namespace std;
+int main(){
+    vector<int> vector;
+    vector.push_back(34);
+    cout<<vector[0];
+    return 0;
+}
