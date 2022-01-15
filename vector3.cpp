0df3753962a55b5085549dd94a4ec404e9113529
asifs-cse <asifs.cse@gmail.com>
2022-01-15 20:52:39 +0530

    vector using iterator

---
+++ vector3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+using namespace std;
+int main(){
+    vector<int> vec;
+    vector<int>::iterator it;
+    for (int i = 0; i < 5; i++)
+    {
+        vec.push_back(i);
+    }
+
+    for (it = vec.begin(); it!=vec.end(); it++)
+    {
+        cout<<*it<<endl;
+    }
+    
+    
+    return 0;
+}
