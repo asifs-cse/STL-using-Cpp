905a0d8a0deeb671d9575148d47b87f196516376
asifs-cse <asifs.cse@gmail.com>
2022-01-15 16:55:32 +0530

    iterator using cpp

--- vector2.cpp
+++ vector2.cpp
@@ -2,11 +2,19 @@
 #include<stdio.h>
 #include<string>
 #include<vector>
+#include<iterator>
 
 using namespace std;
 int main(){
-    vector<int> vect(5,0);
-    for(int i=0; i<vect.size();i++)
-        cout<<vect[i]<<endl;
+    vector<int> vect;
+    vector<int>::iterator it;
+
+    for(int i=0; i<5;i++)
+        vect.push_back(i);
+    
+    it=vect.begin();
+    it++;
+    cout<<*it<<endl;
+
     return 0;
 }
