5762f1a9ae339391a0dcaa1919a0546ad2fbab73
asifs-cse <asifs.cse@gmail.com>
2022-01-17 23:22:32 +0530

    for loop freat

--- vector3.cpp
+++ vector3.cpp
@@ -7,10 +7,10 @@ int main(){
     vector<int>::iterator it; //iterator create
     for (int i = 0; i < 5; i++)
     {
-        vec.push_back(i);
+        vec.push_back(i); // data insert in vector
     }
 
-    for (it = vec.begin(); it!=vec.end(); it++)
+    for (it = vec.begin(); it!=vec.end(); it++) // start for loop
     {
         cout<<*it<<endl;
     }
