e31ad2c0b2c35636483006b26da106bcb3351932
asifs-cse <asifs.cse@gmail.com>
2022-01-21 00:20:13 +0530

    print double vector valu using iterator

--- vectorType.cpp
+++ vectorType.cpp
@@ -10,18 +10,19 @@ int main(){
 
     vector <double> vecd;
     vector<double>::iterator itd;
+    
 
     vector <char> vecc;
     vector <char>::iterator itc;
-    itd = vecd.begin();
 
     for (int i = 0; i < 5; i++)
     {
         vecd.push_back(i); //push data in vector folder
     }
-    for (int i = 0; i < 5; i++)
+    for (itd = vecd.begin(); itd!=vecd.end(); itd++)
     {
-        cout<<vecd[i]<<"\t"; //show output
+        //cout<<vecd[i]<<"\t"<<endl; //show output
+        cout<<*itd<<" \t";
     }
     
 
