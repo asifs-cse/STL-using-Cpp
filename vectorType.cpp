fdbf405a9420d98abbafd1465655af0926d0af88
asifs-cse <asifs.cse@gmail.com>
2022-01-20 23:50:53 +0530

    vector daya type create and also create iterator

---
+++ vectorType.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+
+using namespace std;
+int main(){
+    vector <int> vec;
+    vector <int>::iterator it;
+
+    vector <double> vecd;
+    vector<double>::iterator itd;
+
+    vector <char> vecc;
+    vector <char>::iterator itc;
+    return 0;
+}
