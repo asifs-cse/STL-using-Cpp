1173a7e73ef9fb21637231a0aedf04480082b407
asifs-cse <asifs.cse@gmail.com>
2022-01-22 12:17:33 +0530

    using find algorithm and find data in list using itterator

---
+++ findListItem.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+#include<list>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int arr[5] ={3,2,7,2,5};
+    list<int> myList(arr, arr+5);
+    list<int>::iterator it;
+    
+    it = find(myList.begin(), myList.end(),7);
+
+
+    // it=myList.begin();
+    // it++;
+
+    myList.insert(it, 504);
+    cout<<*it<<endl;
+    for (it= myList.begin(); it!=myList.end(); it++)
+    {
+       cout<<*it<<"\t";
+    }
+    
+    return 0; 
+}
