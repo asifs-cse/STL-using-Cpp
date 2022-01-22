84a5d3fca5c701ad7119f119cd15a5e43714936b
asifs-cse <asifs.cse@gmail.com>
2022-01-22 12:33:20 +0530

    find ellement using find algorithm

---
+++ findDatausingItterator.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    int arr[5], find_s;
+    cout<<"Enter array ellement! "<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>arr[i];
+    }
+    list<int> s_roll(arr, arr+5);
+    list<int>::iterator it;
+    cout<<"Successfully array list data insert to list!"<<endl;
+    for (it=s_roll.begin(); it!=s_roll.end(); it++)
+    {
+        cout<<*it<<"\t";
+    }
+    cout<<"\nEnter found rell no: ";
+    cin>>find_s;
+    it = find(s_roll.begin(), s_roll.end(),find_s);
+    if (it==s_roll.end())
+    {
+        cout<<"Sorry Roll not found! ";
+    }else{
+        cout<<"Roll found"<<endl;
+        cout<<*it;
+    }
+    
+    
+    return 0;
+}
