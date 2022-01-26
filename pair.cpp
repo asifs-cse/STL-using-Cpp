ea07cb84ac3b9bdb4755ee665419d07df476b5ae
asifs-cse <asifs.cse@gmail.com>
2022-01-26 10:34:25 +0530

    difrent way to insert data in pair

--- pair.cpp
+++ pair.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 int main(){
     pair<string, int> mPair("asif", 501);
+    //difrent way to insert data
+    mPair = make_pair("raje", 501);
+
+    //difrent way to inser data in pair
+    mPair.first = "tamzid";
+    mPair.second = 536;
     cout<<mPair.first<<" "<<mPair.second;
     return 0;
 }
