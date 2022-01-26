b689ec5152d8d93f863df5ba315676b67c1377f7
asifs-cse <asifs.cse@gmail.com>
2022-01-26 11:03:25 +0530

    print all element using iterator

--- vectorUsingPair.cpp
+++ vectorUsingPair.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 int main(){
     vector<pair<int, string>> myVec;
+    //create iterator
+    vector<pair<int, string>>:: iterator vit;
 
     myVec.push_back(make_pair(504,"asif"));
     myVec.push_back(make_pair(501,"raja"));
 
     cout<<myVec[0].first<<" "<<myVec[0].second;
+
+    cout<<"print all vecotr element: "<<endl;
+
+    for (vit =myVec.begin(); vit!=myVec.end(); vit++)
+    {
+        cout<<vit->first<<" "<<vit->second<<endl;
+    }
+    
+
+
     return 0;
 }
