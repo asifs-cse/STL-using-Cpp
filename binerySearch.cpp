e244531e73c95b4fff9b68dfc73d3cda158385fb
asifs-cse <asifs.cse@gmail.com>
2022-01-27 00:27:58 +0530

    using lowerbound

--- binerySearch.cpp
+++ binerySearch.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<iterator>
 #include<algorithm>
 using namespace std;
 
 int main(){
-    vector<int> v={1,34,2,4,2,5};
+    vector<int> v={1,34,2,4,2,6};
     bool f =binary_search(v.begin(), v.end(), 5);
 
-    if(f) cout<<"found";
-    else cout<<"not found";
+    if(f) cout<<"found"<<endl;
+    else cout<<"not found"<<endl;
+
+    //lower bound
+    vector<int>:: iterator it = lower_bound(v.begin(), v.end(), 5);
+    cout<<*it<<endl;
+
+    
     return 0;
 }
