515e40c4d642665417cdcf05b14ac1e640e132d1
asifs-cse <asifs.cse@gmail.com>
2022-01-26 10:25:07 +0530

    show all key and value using iterator

--- iteratorUsingMap.cpp
+++ iteratorUsingMap.cpp
@@ -8,19 +8,21 @@ int main(){
     map<string, int> myMap;
     map<string, int>::iterator mit;
 
-    myMap.insert(make_pair("asif",504));
     myMap.insert(make_pair("tamzid",536));
     myMap.insert(make_pair("raja", 501));
+    myMap.insert(make_pair("asif",504));
 
     mit = myMap.begin();
     mit++;
     cout<<mit->first<<endl;
     //output key value
     cout<<mit->second<<endl;
-    // for (int mit=myMap.begin(); myMap!=myMap.end(); mit++)
-    // {
-    //     /* code */
-    // }
+
+    //show all value
+    for (mit=myMap.begin(); mit!=myMap.end(); mit++)
+    {
+        cout<<mit->first<<" "<<mit->second<<endl;
+    }
     
     return 0;
 }
