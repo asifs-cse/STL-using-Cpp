2ca6b80a6b909de266606a1eaf4e88d2541bbd9d
asifs-cse <asifs.cse@gmail.com>
2022-01-27 19:59:00 +0530

    pass string in bitset

--- bitSet.cpp
+++ bitSet.cpp
@@ -2,9 +2,9 @@
 #include<bitset>
 using namespace std;
 int main(){
-    bitset<8> b;
-    b.set();
-    b.reset();
-    cout<<b;
+    bitset<8> b(string("1101"));
+    // b.set();
+    // b.reset();
+    cout<<b<<endl<<b.size();
     return 0;
 }
