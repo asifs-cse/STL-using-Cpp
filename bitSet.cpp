d8ec74dcd875753b28ecce27ad82af6a3b800e6d
asifs-cse <asifs.cse@gmail.com>
2022-01-27 20:32:51 +0530

    count set bit

--- bitSet.cpp
+++ bitSet.cpp
@@ -5,6 +5,7 @@ int main(){
     bitset<8> b(string("1101"));
     // b.set();
     // b.reset();
-    cout<<b<<endl<<b.size();
+    cout<<b<<endl<<b.size()<<endl;
+    cout<<b.count(); //find set bit
     return 0;
 }
